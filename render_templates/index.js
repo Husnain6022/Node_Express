ad160ad393e5de7e164507058078e82368d8c644
Husnain <ihusnain844@gmail.com>
2024-07-27 01:43:45 -0700

    dynamic tempaltes with ejs

---
+++ render_templates/index.js
@@ -0,0 +1,30 @@
+import express, { response } from "express";
+
+
+const app = express();
+const port = 3000;
+
+app.get("/", (request, response) => {
+    const x = new Date();
+    const today = x.getDay();
+
+    console.log(today);
+    let day = 'weekday';
+    let advice = "it's time to work hard";
+
+    if ( today === 0 || today === 6) {
+        day = "Weekend";
+        advice = "it's to enjoy the weekend, Hurraaha !!! "
+    }
+
+    response.render("index.ejs", {
+        day: day,
+        advice: advice
+    })
+})
+
+
+app.listen(port, () => {
+    console.log(`The server is listening at ${port}`)
+})
+
