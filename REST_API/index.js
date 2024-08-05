a196a9f86a40539f0120e41981d05068d769e554
Husnain Iftikhar <ihusnain844@gmail.com>
2024-08-05 10:52:42 +0500

    REST API

---
+++ REST_API/index.js
@@ -0,0 +1,71 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+const yourBearerToken = "a7e27b99-e3aa-4293-8a8c-affbcb6ff2a2";
+const config = {
+  headers: { Authorization: `Bearer ${yourBearerToken}` },
+};
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "Waiting for data..." });
+});
+
+app.post("/get-secret", async (req, res) => {
+  const searchId = req.body.id;
+  try {
+    const response = await axios.get(API_URL + "/secrets/" + searchId, config);
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
+});
+
+app.post("/post-secret", async (req, res) => {
+  try{
+    const response = await axios.post(API_URL+ "/secrets", req.body ,config)
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
+  });
+
+app.post("/put-secret", async (req, res) => {
+  try{
+  const searchId = req.body.id;
+  const response = await axios.put(API_URL + "/secrets/" + searchId , req.body ,config)
+  res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
+});
+
+app.post("/patch-secret", async (req, res) => {
+  try {
+  const searchId = req.body.id;
+  const response = await axios.patch(API_URL + "/secrets/" + searchId , req.body, config)
+  res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
+});
+
+app.post("/delete-secret", async (req, res) => {
+  try{
+  const searchId = req.body.id;
+  const response = await axios.delete(API_URL + "/secrets/" + searchId, config)
+  res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
